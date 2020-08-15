4758193f5d8a1ce2542a411957d9fb8aa04d1cc9
CodingSeaOtter <CodingSeaOtter@gmail.com>
2020-08-15 17:46:55 -0400

    Added default option capabilities

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+const {mergeDeep} = require('./utilities/index.js');
+const DEFAULT_OPTIONS = {};
+
 class HinataHyuga {
     constructor(options = {}, logger) {
-        this.options = options;
+        this.options = mergeDeep(DEFAULT_OPTIONS, options);
         this.logger = logger;
     }
 
